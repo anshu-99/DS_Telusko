7eab87c8d7005e62b19d2d363fa1d56e68507441
anshu99 <udbhavsbg@gmail.com>
2025-05-03 22:59:22 +0530

    DS_codes

---
+++ src/ArrayCodes/BasicArrayCode.java
@@ -0,0 +1,40 @@
+package ArrayCodes;
+import java.util.*;
+public class BasicArrayCode {
+    public static void arrayInput(int[] arr){
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+//        return arr;
+    }
+    public static void arrayOutput(int[] arr){
+        for(Integer i:arr){
+            System.out.print(i+" ");
+        }
+//        return arr;
+    }
+    public static void reverseArray(int arr[]){
+        int[] reversedArray=new int[arr.length];
+        for(int i=0;i<arr.length;){
+            for(int j=arr.length-1;j>=0;j--){
+                reversedArray[j]=arr[i];
+                i++;
+            }
+        }
+        BasicArrayCode.arrayOutput(reversedArray);
+//        return reversedArray;
+
+    }
+    public static void main(String[] args) {
+        int[] arr=new int[10];
+        System.out.print("Enter data:");
+        BasicArrayCode.arrayInput(arr);
+        System.out.println();
+        System.out.print("Your Data:");
+        BasicArrayCode.arrayOutput(arr);
+        System.out.println();
+        System.out.println("Reversed Array:");
+        BasicArrayCode.reverseArray(arr);
+    }
+}
