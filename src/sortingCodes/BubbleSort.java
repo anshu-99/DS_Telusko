2bce678f925e7f0f818e270f8baea7108e642191
anshu99 <udbhavsbg@gmail.com>
2025-05-04 13:45:49 +0530

    DS_codes

--- src/sortingCodes/BubbleSort.java
+++ src/sortingCodes/BubbleSort.java
@@ -7,27 +7,30 @@ public class BubbleSort {
         }
     }
     public static void bubbleSort(int[] arr) {
-        int temp = 0;
-        for (int i = 0; i<arr.length-1; i++) {
-            for(int j=0;j<arr.length-i-1;j++) {
-                if (arr[j] > arr[j+1]) {
+        int n = arr.length;
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j + 1]
                     temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-                System.out.println();
-                for (Integer num : arr) {
-                    System.out.print(+num + " ");
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
+            // Print array after each pass
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
-        System.out.println("After Sorting");
+        System.out.println("After Sorting:");
         BubbleSort.printArray(arr);
     }
 
+
     public static void main(String[] args) {
-        int[] arr={11,89,10,54,32,90,21,45};
+        int[] arr={11,89,10,54,32,90,21,45,111,151,100,1,890,141,200};
         System.out.println("Before sorting");
         BubbleSort.printArray(arr);
         System.out.println();
